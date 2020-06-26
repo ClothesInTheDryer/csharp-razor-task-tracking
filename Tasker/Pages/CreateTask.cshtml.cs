ef67bf4648c81c112bdab879a64916486d5d40e8
ClothesInTheDryer <pieceofplastico@gmail.com>
2020-06-25 22:43:07 -0700

    ...

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -21,6 +21,7 @@ namespace Tasker.Pages
         public IActionResult OnPost()
         {
             _context.Tasks.Add(NewTask);
+            _context.SaveChanges();
             return RedirectToPage("Index");
         }
         public void OnGet()
